3a325a778f4f7dd7781670463b3cc2f0534f05b3
t_okamoto <t_okamoto@freestyles.jp>
2021-01-22 21:57:41 +0900

    初期配置

---
+++ index.js
@@ -0,0 +1,20 @@
+let { app, BrowserWindow, screen } = require("electron");
+
+const createWindow = () => {
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+    window = new BrowserWindow({
+        width: width / 1.25,
+        height: height / 1.25,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    window.loadURL('https://google.com');
+};
+
+let window = null;
+
+app.whenReady().then(createWindow)
+app.on('window-all-closed', () => app.quit());
